0c6960c65a57def71d032f89d86aaca89d769e28
Sara <122972681+Sarajona@users.noreply.github.com>
2025-10-11 14:25:22 +0200

    added outline file

---
+++ features/steps/shopping_cart_steps.py
@@ -0,0 +1,149 @@
+from behave.api.pending_step import StepNotImplementedError
+@given(u'the user is on the details page of a specific book')
+def step_impl(context):
+    raise StepNotImplementedError(u'Given the user is on the details page of a specific book')
+
+
+@when(u'the user clicks on the add to chart button')
+def step_impl(context):
+    raise StepNotImplementedError(u'When the user clicks on the add to chart button')
+
+
+@then(u'the book should be added to the shopping cart')
+def step_impl(context):
+    raise StepNotImplementedError(u'Then the book should be added to the shopping cart')
+
+
+@given(u'the user is viweing the shopping cart')
+def step_impl(context):
+    raise StepNotImplementedError(u'Given the user is viweing the shopping cart')
+
+
+@when(u'the user clicks on the remove from cart button of a specific book')
+def step_impl(context):
+    raise StepNotImplementedError(u'When the user clicks on the remove from cart button of a specific book')
+
+
+@then(u'the book should be removed from the shopping cart')
+def step_impl(context):
+    raise StepNotImplementedError(u'Then the book should be removed from the shopping cart')
+
+
+@given(u'the shopping cart contains 0 books with individual prices as in []')
+def step_impl(context):
+    raise StepNotImplementedError(u'Given the shopping cart contains 0 books with individual prices as in []')
+
+
+@when(u'the user opens the shopping cart')
+def step_impl(context):
+    raise StepNotImplementedError(u'When the user opens the shopping cart')
+
+
+@then(u'the displayed total should equal 0.00')
+def step_impl(context):
+    raise StepNotImplementedError(u'Then the displayed total should equal 0.00')
+
+
+@then(u'the book count is 0')
+def step_impl(context):
+    raise StepNotImplementedError(u'Then the book count is 0')
+
+
+@given(u'the shopping cart contains 1 books with individual prices as in [120.00]')
+def step_impl(context):
+    raise StepNotImplementedError(u'Given the shopping cart contains 1 books with individual prices as in [120.00]')
+
+
+@then(u'the displayed total should equal 120.00')
+def step_impl(context):
+    raise StepNotImplementedError(u'Then the displayed total should equal 120.00')
+
+
+@then(u'the book count is 1')
+def step_impl(context):
+    raise StepNotImplementedError(u'Then the book count is 1')
+
+
+@given(u'the shopping cart contains 3 books with individual prices as in [50.00, 75.00, 25.00]')
+def step_impl(context):
+    raise StepNotImplementedError(u'Given the shopping cart contains 3 books with individual prices as in [50.00, 75.00, 25.00]')
+
+
+@then(u'the displayed total should equal 150.00')
+def step_impl(context):
+    raise StepNotImplementedError(u'Then the displayed total should equal 150.00')
+
+
+@then(u'the book count is 3')
+def step_impl(context):
+    raise StepNotImplementedError(u'Then the book count is 3')
+
+
+@given(u'the user is on the details page of a book that already has 0 copies of in the shopping cart')
+def step_impl(context):
+    raise StepNotImplementedError(u'Given the user is on the details page of a book that already has 0 copies of in the shopping cart')
+
+
+@when(u'the user clicks on the add to cart button')
+def step_impl(context):
+    raise StepNotImplementedError(u'When the user clicks on the add to cart button')
+
+
+@then(u'The copy count is 1')
+def step_impl(context):
+    raise StepNotImplementedError(u'Then The copy count is 1')
+
+
+@given(u'the user is on the details page of a book that already has 1 copies of in the shopping cart')
+def step_impl(context):
+    raise StepNotImplementedError(u'Given the user is on the details page of a book that already has 1 copies of in the shopping cart')
+
+
+@then(u'The copy count is 2')
+def step_impl(context):
+    raise StepNotImplementedError(u'Then The copy count is 2')
+
+
+@given(u'the user is on the details page of a book that already has 51 copies of in the shopping cart')
+def step_impl(context):
+    raise StepNotImplementedError(u'Given the user is on the details page of a book that already has 51 copies of in the shopping cart')
+
+
+@then(u'The copy count is 52')
+def step_impl(context):
+    raise StepNotImplementedError(u'Then The copy count is 52')
+
+
+@given(u'the user is viewing the shopping cart')
+def step_impl(context):
+    raise StepNotImplementedError(u'Given the user is viewing the shopping cart')
+
+
+@given(u'the shopping cart has 0 books')
+def step_impl(context):
+    raise StepNotImplementedError(u'Given the shopping cart has 0 books')
+
+
+@when(u'the user clicks on the clear cart button')
+def step_impl(context):
+    raise StepNotImplementedError(u'When the user clicks on the clear cart button')
+
+
+@then(u'the total price is 0')
+def step_impl(context):
+    raise StepNotImplementedError(u'Then the total price is 0')
+
+
+@given(u'the shopping cart has 1 books')
+def step_impl(context):
+    raise StepNotImplementedError(u'Given the shopping cart has 1 books')
+
+
+@given(u'the shopping cart has 5 books')
+def step_impl(context):
+    raise StepNotImplementedError(u'Given the shopping cart has 5 books')
+
+
+@given(u'the shopping cart has 10 books')
+def step_impl(context):
+    raise StepNotImplementedError(u'Given the shopping cart has 10 books')
